5b88433dd2b819ff77e2c11d4346786108f12409
Induwara <induwara2k@gmail.com>
2024-08-11 10:25:38 +0530

    feat: prototype scope

--- Spring-Introduction/src/main/java/lk/ijse/gdse68/AppInit.java
+++ Spring-Introduction/src/main/java/lk/ijse/gdse68/AppInit.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse68;
 
+import lk.ijse.gdse68.beans.TestBeans;
 import lk.ijse.gdse68.config.Config;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,11 +13,18 @@ public class AppInit {
         context.register(Config.class); //Put the class into the Bucket
         context.refresh(); //Refresh (shake) the bucket to make the class visible to the spring.
 
+        TestBeans test = (TestBeans) context.getBean("test");
+
         //context.close();
 
         //Get bean factory from the context
         ConfigurableBeanFactory beanFactory = context.getBeanFactory();
-        boolean isSingleCustomer = beanFactory.isSingleton("customer");
+
+        System.out.println("test = " + test);
+
+
+        //Non of the method of the TestBeans class will be called 
+        boolean isSingleCustomer = beanFactory.isSingleton("test");
         System.out.println("isSingleCustomer = " + isSingleCustomer);
         context.registerShutdownHook();
 
--- Spring-Introduction/src/main/java/lk/ijse/gdse68/beans/TestBeans.java
+++ Spring-Introduction/src/main/java/lk/ijse/gdse68/beans/TestBeans.java
@@ -4,14 +4,17 @@ package lk.ijse.gdse68.beans;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //You can use this to give a name to the bean
 //If you did not give a name to the bean it will take the class name as the bean name
 //Eg: TestBeans -> testBeans
-@Component("TestBeans")
+@Component("test")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TestBeans implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
 
