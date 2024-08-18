2f5eb2c3138cdb5710aa693c6fcd2ff6995d95b8
Induwara <induwara2k@gmail.com>
2024-08-18 09:45:12 +0530

    feat: add context path

--- introduction-spring-web/src/main/java/lk/ijse/gdse68/introductionspringweb/controller/DemoController.java
+++ introduction-spring-web/src/main/java/lk/ijse/gdse68/introductionspringweb/controller/DemoController.java
@@ -1,8 +1,6 @@
 package lk.ijse.gdse68.introductionspringweb.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/demo")
 @RestController
@@ -10,9 +8,34 @@ public class DemoController {
 
     @GetMapping(value = "/health")
     public String healthCheck(){
+        System.out.println("GG");
         return "DemoController run perfectly";
     }
 
+
+    @GetMapping(value = "/pattern/{name}")
+    public String pathVariable(@PathVariable ("name") String name) {
+        return "DemoController run perfectly with :"+name;
+    }
+    @GetMapping(value = "/patternRegex/{id:C\\d{3}}")
+    public String pathvariableWithRegex(@PathVariable("id") String id) {
+        return "DemoController run perfectly with :"+id;
+    }
+
+    @GetMapping(value = "/patternRegex/{id:C\\d{3}}",headers = "X-number")
+    public String pathvariableWithRegexAndHeader(@PathVariable("id") String id,@RequestHeader("X-number") int num) {
+        return "Path variable is :"+id + " and Header is :"+num;
+    }
+
+    @GetMapping(params = "test=all")
+    public String params() {
+        return "All are tested";
+    }
+
+    @PostMapping(params = {"name","quantity"})
+    public String paramData(@RequestParam("name") String name,@RequestParam String quantity) {
+        return "Name : "+name+" Quantity : "+quantity;
+    }
 
 
 }
