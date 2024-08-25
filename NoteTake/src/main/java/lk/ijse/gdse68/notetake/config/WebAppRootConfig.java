4970282759f9a95f2e492f3d1e12c38afb0e8782
Induwara <induwara2k@gmail.com>
2024-08-25 16:29:37 +0530

    feat: configure model mapper

--- NoteTake/src/main/java/lk/ijse/gdse68/notetake/config/WebAppRootConfig.java
+++ NoteTake/src/main/java/lk/ijse/gdse68/notetake/config/WebAppRootConfig.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse68.notetake.config;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +23,10 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 public class WebAppRootConfig {
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     @Bean
     public DataSource dataSource(){
