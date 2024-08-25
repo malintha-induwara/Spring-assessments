86af2d1b8097fdb1830c3e6828a36ec2e9958d44
Induwara <induwara2k@gmail.com>
2024-08-25 21:03:26 +0530

    feat: implement crud methods

--- NoteTake/src/main/java/lk/ijse/gdse68/notetake/service/NoteServiceImpl.java
+++ NoteTake/src/main/java/lk/ijse/gdse68/notetake/service/NoteServiceImpl.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse68.notetake.service;
 
 import lk.ijse.gdse68.notetake.dao.NoteDao;
 import lk.ijse.gdse68.notetake.dto.NoteDTO;
+import lk.ijse.gdse68.notetake.entity.Note;
 import lk.ijse.gdse68.notetake.util.AppUtil;
 import lk.ijse.gdse68.notetake.util.Mapping;
 import org.modelmapper.ModelMapper;
@@ -32,6 +33,13 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public boolean updateNote(String noteId, NoteDTO noteDTO) {
+
+        Note update = noteDao.save(mapping.convertToNoteEntity(noteDTO));
+        if (update != null) {
+            return true;
+        }
+
+
 
 
         //We dont use following way because when updating data from a running list it can throw
@@ -65,6 +73,9 @@ public class NoteServiceImpl implements NoteService {
     @Override
     public boolean deleteNote(String noteId) {
 
+        noteDao.findById(noteId).ifPresent(note -> noteDao.delete(note));
+
+
 //        for (NoteDTO noteDTO : saveNoteTmp) {
 //            if (noteDTO.getNoteId().equals(noteId)) {
 //                saveNoteTmp.remove(noteDTO);
@@ -87,6 +98,10 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public NoteDTO getSelectedNote(String noteId) {
+        Note note = noteDao.findById(noteId).orElse(null);
+        if (note != null) {
+            return mapping.convertToNoteDTO(note);
+        }
 //        for (NoteDTO noteDTO : saveNoteTmp) {
 //            if (noteDTO.getNoteId().equals(noteId)) {
 //                return noteDTO;
@@ -97,7 +112,8 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public List<NoteDTO> getAllNotes() {
+        List<Note> all = noteDao.findAll();
+        return mapping.convertToNoteDTOList(all);
 //        return saveNoteTmp;
-        return null;
     }
 }
