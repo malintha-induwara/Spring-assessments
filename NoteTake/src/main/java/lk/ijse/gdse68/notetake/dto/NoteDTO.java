c95430a37c1f50e6503317877a5eff452adfea90
Induwara <induwara2k@gmail.com>
2024-08-25 16:24:00 +0530

    feat: configure hibernate and entities

--- NoteTake/src/main/java/lk/ijse/gdse68/notetake/dto/NoteDTO.java
+++ NoteTake/src/main/java/lk/ijse/gdse68/notetake/dto/NoteDTO.java
@@ -1,75 +1,20 @@
 package lk.ijse.gdse68.notetake.dto;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NoteDTO implements Serializable {
     private String noteId;
     private String noteTitle;
     private String noteDescription;
     private String priorityLevel;
     private String createDate;
-
-
-    public NoteDTO() {
-    }
-
-    public NoteDTO(String noteId, String noteTitle, String noteDescription, String priorityLevel, String createDate) {
-        this.noteId = noteId;
-        this.noteTitle = noteTitle;
-        this.noteDescription = noteDescription;
-        this.priorityLevel = priorityLevel;
-        this.createDate = createDate;
-    }
-
-    public String getNoteId() {
-        return noteId;
-    }
-
-    public void setNoteId(String noteId) {
-        this.noteId = noteId;
-    }
-
-    public String getNoteTitle() {
-        return noteTitle;
-    }
-
-    public void setNoteTitle(String noteTitle) {
-        this.noteTitle = noteTitle;
-    }
-
-    public String getNoteDescription() {
-        return noteDescription;
-    }
-
-    public void setNoteDescription(String noteDescription) {
-        this.noteDescription = noteDescription;
-    }
-
-    public String getPriorityLevel() {
-        return priorityLevel;
-    }
-
-    public void setPriorityLevel(String priorityLevel) {
-        this.priorityLevel = priorityLevel;
-    }
-
-    public String getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(String createDate) {
-        this.createDate = createDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Note{" +
-                "noteId='" + noteId + '\'' +
-                ", noteTitle='" + noteTitle + '\'' +
-                ", noteDescription='" + noteDescription + '\'' +
-                ", priorityLevel='" + priorityLevel + '\'' +
-                ", createDate='" + createDate + '\'' +
-                '}';
-    }
 }
 
--- NoteTake/src/main/java/lk/ijse/gdse68/notetake/entity/Note.java
+++ NoteTake/src/main/java/lk/ijse/gdse68/notetake/entity/Note.java
@@ -1,3 +1,23 @@
-package lk.ijse.gdse68.notetake.entity;public class Note {
+package lk.ijse.gdse68.notetake.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "notes")
+@Entity
+public class Note {
+    @Id
+    private String noteId;
+    private String noteTitle;
+    private String noteDescription;
+    private String priorityLevel;
+    private String createDate;
 }
 
