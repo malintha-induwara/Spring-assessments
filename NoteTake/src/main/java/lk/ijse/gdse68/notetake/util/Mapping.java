e1371af86e2dc22246377dbd4d9fa73270d88c6b
Induwara <induwara2k@gmail.com>
2024-09-01 15:27:54 +0530

    feat(mapping): implement user mappings

--- NoteTake/src/main/java/lk/ijse/gdse68/notetake/util/Mapping.java
+++ NoteTake/src/main/java/lk/ijse/gdse68/notetake/util/Mapping.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse68.notetake.util;
 
 import lk.ijse.gdse68.notetake.dto.NoteDTO;
+import lk.ijse.gdse68.notetake.dto.UserDTO;
 import lk.ijse.gdse68.notetake.entity.Note;
+import lk.ijse.gdse68.notetake.entity.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,12 +19,20 @@ public class Mapping {
     public NoteDTO convertToNoteDTO(Note note) {
         return modelMapper.map(note, NoteDTO.class);
     }
-
     public Note convertToNoteEntity(NoteDTO noteDTO) {
         return modelMapper.map(noteDTO, Note.class);
     }
-
     public List<NoteDTO> convertToNoteDTOList(List<Note> list) {
+        return modelMapper.map(list, List.class);
+    }
+
+    public UserDTO convertToUserDTO(User user) {
+        return modelMapper.map(user, UserDTO.class);
+    }
+    public User convertToUserEntity(UserDTO userDTO) {
+        return modelMapper.map(userDTO, User.class);
+    }
+    public List<UserDTO> convertToUserDTOList(List<User> list) {
         return modelMapper.map(list, List.class);
     }
 
